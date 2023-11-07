f8f8364fe091958b2b899caa8d7e811ec48c6ab0
onelonelyowl <64985946+onelonelyowl@users.noreply.github.com>
2023-11-07 20:20:04 +0000

    part 1 completed

--- App.js
+++ App.js
@@ -1,17 +1,15 @@
 import React from 'react';
-// Import data and WeatherCard here
+import WeatherCard from './components/WeatherCard';
+const cities = require('./data.js')
 
-
+// console.log(cities)
 function App() {
     return (
         <>
             <h1 className = "title">REACTIVE WEATHER</h1>
             <h3 className = "subtitle">Up to the minute weather news</h3>
             <div className = "app">
-                {/* Render components here */}
-                
-
-
+                {cities.map(city => <WeatherCard city={city} />)}
             </div>
             
         </>
--- components/WeatherCard.js
+++ components/WeatherCard.js
@@ -2,19 +2,19 @@ import React from "react";
 import sunny from "../assets/Sunny.svg"
 
 
-function WeatherCard() {
+function WeatherCard(props) {
   return (
     <div className = "card">
         <div className = "img-container">
             <img className="card-img-top" src = {sunny} alt="Card image cap" id = "icon"/>
         </div>
         <div class="card-body">
-            <h3 className="card-title">CITY DATA HERE</h3>
-            <h5 className="card-text">TEMPERATURE DATA HERE</h5>
-            <h5 className="card-text">FORECAST DATA HERE</h5>
+            <h3 className="card-title">City: {props.city.city}</h3>
+            <h5 className="card-text">Temperature: {props.city.temperature}</h5>
+            <h5 className="card-text">Forecast: {props.city.forecast}</h5>
         </div>
     </div>
   );
 };
 
-// Export the WeatherCard
+export default WeatherCard;
